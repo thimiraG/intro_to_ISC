2c4c5c2a8433c1641d513b2d49762b45421adc19
thimiraG <gimhan.thimee@gmail.com>
2021-07-26 16:40:43 +0530

    sixth ccommit

---
+++ script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+import sys
+import hashlib
+
+
+
+
+arg1_bytes = bytes(sys.argv[1], 'utf-8')
+dk = hashlib.pbkdf2_hmac('sha512',arg1_bytes,b'km5d5ivMy8iexuHcZrsD',200000)
+
+
+print ('output', dk.hex())
+
+
